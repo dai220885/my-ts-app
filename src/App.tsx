d245d5e8c46d07ca74aa5424b091a7cd4b4d3108
Alexandr Dylevich <dylevich220885@gmail.com>
2023-01-26 18:53:31 +0300

    add components Accordion and Raiting

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Raiting} from "./components/Raiting/Raiting";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log("App rendering")
+    //делает что-то полезное
+    //обязана вернуть JSX
+    return (
+        <div>
+            <PageTitle title={"Page title 1"}/>
+            <PageTitle title={"Page title 2"}/>
+            <PageTitle title={"Page title 3"}/>
+            <PageTitle title={"Page title 4"}/>
+            <Raiting value={2}/>
+            <Accordion titleValue={"Menu"} collapsed={false}/>
+            <Accordion titleValue={"Users"} collapsed={true}/>
+            <Accordion titleValue={"Settings"} collapsed={false}/>
+            <Raiting value={0}/>
+            <Raiting value={1}/>
+            <Raiting value={2}/>
+            <Raiting value={3}/>
+            <Raiting value={4}/>
+            <Raiting value={5}/>
+        </div>
+    );
 }
 
 export default App;
+
+type PageTitlePropsType = {
+    title: string;
+}
+
+function PageTitle(props: PageTitlePropsType) {
+    console.log("PageTitle rendering")
+    return (
+        <h1>{props.title}</h1>
+    );
+}
+
+
+
