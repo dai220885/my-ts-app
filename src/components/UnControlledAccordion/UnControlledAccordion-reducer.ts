171080aadfa6c954caf9fac9d9ae9ec13d097109
Alexandr Dylevich <dylevich220885@gmail.com>
2023-03-26 13:48:00 +0300

    реализован редьюсер для неконтролируемого аккордиона и тесты длдя него (вторник, неделя 3 "16 - хук useReducer")

---
+++ src/components/UnControlledAccordion/UnControlledAccordion-reducer.ts
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const TOGGLE_COLLAPSED = 'TOGGLE-COLLAPSED'
+export type AccStateType = {
+    collapsed: boolean
+}
+
+const UnControlledAccordionReducer = (state: AccStateType, action: ToggleCollapsedActionType) => {
+    switch (action.type){
+        case TOGGLE_COLLAPSED:
+            return {...state, collapsed: !state.collapsed}
+        default: return state
+    }
+
+};
+
+
+
+
+export type ToggleCollapsedActionType = ReturnType<typeof toggleCollapsedAC>
+export const toggleCollapsedAC = () => ({type: TOGGLE_COLLAPSED} as const)
+
+
+export default UnControlledAccordionReducer;
+
